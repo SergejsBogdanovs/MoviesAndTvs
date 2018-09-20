7b064dc32688d1eb6758fc68533f91e43bb15bfb
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2018-09-20 09:03:22 +0300

    Implemented StartupActivity functionality

--- data/src/main/java/lv/st/sbogdano/data/remote/api/CinemaService.kt
+++ data/src/main/java/lv/st/sbogdano/data/remote/api/CinemaService.kt
@@ -1,5 +1,6 @@
 package lv.st.sbogdano.data.remote.api
 
+import com.serjltt.moshi.adapters.Wrapped
 import io.reactivex.Observable
 import lv.st.sbogdano.data.remote.model.GenreTypeRemoteModel
 import retrofit2.http.GET
@@ -7,6 +8,7 @@ import retrofit2.http.GET
 interface CinemaService {
 
     @GET("genre/movie/list")
+    @Wrapped(path = ["genres"])
     fun getGenreTypes(): Observable<List<GenreTypeRemoteModel>>
 
 }
